b8dc15f007caaedb0b10fad483bcaf3f5d48b20a
Anand Satheesh <anands282@gmail.com>
2024-05-31 21:42:44 +0530

    updated graph functions

---
+++ linkedlist/linkedlist.py
@@ -0,0 +1,40 @@
+from node import Node
+
+class LinkedList:
+    def __init__(self, value):
+        new_node = Node(value)
+        self.head = new_node
+        self.tail = new_node
+        self.length = 1
+
+    def append(self, value):
+        new_node = Node(value)
+        if self.head is None:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            self.tail.next = new_node
+            self.tail = new_node
+        self.length +=1
+        return True
+
+    def prepend(self, value):
+
+    def insert(self,index, value):
+
+    def pop(self):
+        if self.length == 0:
+            return None
+        temp = self.head
+        pre = self.head
+        while(temp.next):
+            pre = temp
+            temp = temp.next
+        self.tail = pre
+        self.tail.next = None
+
+    def print_list(self):
+        temp = self.head
+        while temp is not None:
+            print(temp.value)
+            temp = temp.next
