70580271bc1e33c93755742a551c550e41d4a555
Anand Satheesh <anands282@gmail.com>
2024-05-31 15:09:35 +0530

    updated graph functions

--- graph/graph.py
+++ graph/graph.py
@@ -2,6 +2,10 @@ class Graph:
     def __init__(self):
         self.adj_list = {}
 
+    def print_graph(self):
+        for vertex in self.adj_list:
+            print(vertex, ':', self.adj_list[vertex])
+
     def add_vertex(self, vertex):
         if vertex not in self.adj_list.keys():
             self.adj_list[vertex] = []
@@ -14,3 +18,34 @@ class Graph:
             self.adj_list[vertex2].append(vertex1)
             return True
         return False
+
+    def remove_edge(self, vertex1, vertex2):
+        if vertex1 in self.adj_list.keys() and vertex2 in self.adj_list.keys():
+            try:
+                self.adj_list[vertex1].remove(vertex2)
+                self.adj_list[vertex2].remove(vertex1)
+            except ValueError:
+                pass
+            return True
+        return False
+
+    def remove_vertex(self, vertex):
+        if vertex in self.adj_list.keys():
+            for other_vertex in self.adj_list[vertex]:
+                self.adj_list[other_vertex].remove(vertex)
+            del self.adj_list[vertex]
+            return True
+        return False
+
+
+if __name__ == "__main__":
+    my_graph = Graph()
+    my_graph.add_vertex("A")
+    my_graph.add_vertex("B")
+    my_graph.add_vertex("C")
+    my_graph.add_vertex("D")
+
+    my_graph.add_edge("A", "B")
+    my_graph.add_edge("B", "C")
+    my_graph.add_edge("C", "A")
+    my_graph.print_graph()
