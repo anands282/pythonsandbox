010fe15154733298b7f7885f00e5e54afe08e9ba
Anand Satheesh <anands282@gmail.com>
2024-06-04 09:22:15 +0530

    updated the deque

--- dsa/last_n_items.py
+++ dsa/last_n_items.py
@@ -1,10 +1,20 @@
 from collections import deque
 
+
 def search(lines, pattern, history =5):
     previous_lines = deque(maxlen=history)
-    #creates a fixed length queue when new items are added
+    # creates a fixed length queue when new items are added
     for line in lines:
         if pattern in line:
             yield line, previous_lines
         previous_lines.append(line)
+
+
+if __name__ == "__main__":
+    with open('sample_file.txt') as f:
+        for line, previous_lines in search(f, "python", 5):
+            for pline in previous_lines:
+                print(pline, end="")
+            print(line, end="")
+            print('-'*20)
 
