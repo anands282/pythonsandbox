2840ae03f3a6f9280880fde557d33e8b5324b978
Anand Satheesh <anands282@gmail.com>
2024-06-09 19:04:35 +0530

    updated insert function

--- dsa/queue/queue.py
+++ dsa/queue/queue.py
@@ -5,3 +5,14 @@ class Queue(object):
         self.__front = 1
         self.__rear = 0
         self.__num_items = 0
+
+    def insert(self, item):
+        if self.isFull():
+            raise Exception("Queue overflow")
+        self.__rear += 1
+        if self.__rear == self.maxSize:
+            self.__rear = 0
+        self.__que[self.__rear] = item
+        self.__num_items += 1
+        return True
+
