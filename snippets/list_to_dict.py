e694a193f638dea437789f6f0edf50008585a227
Anand Satheesh <anands282@gmail.com>
2024-06-30 21:51:24 +0530

    updated snip

--- snippets/list_to_dict.py
+++ snippets/list_to_dict.py
@@ -10,3 +10,10 @@ print(dict_method1)
 dict_method2 = {key: value for key, value in zip(key_lists, value_lists)}
 
 print(dict_method2)
+
+item_tuples = zip(key_lists, value_lists)
+dict_method3= {}
+for key, value in item_tuples:
+    dict_method3[key] = value
+
+print(dict_method3)
