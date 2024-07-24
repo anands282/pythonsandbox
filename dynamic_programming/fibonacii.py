9d7c16e513096f4046026be30dcaf581511489b0
Anand Satheesh <anands282@gmail.com>
2024-07-24 20:28:43 +0530

    added tabulation approach

--- dynamic_programming/fibonacii.py
+++ dynamic_programming/fibonacii.py
@@ -8,3 +8,17 @@ def fibonacci_memo(n, memo={}):
 
 # Example usage:
 print(fibonacci_memo(10))  # Output: 55
+
+
+
+def fibonacci_tab(n):
+    if n <= 1:
+        return n
+    fib = [0] * (n + 1)
+    fib[1] = 1
+    for i in range(2, n + 1):
+        fib[i] = fib[i-1] + fib[i-2]
+    return fib[n]
+
+# Example usage:
+print(fibonacci_tab(10))  # Output: 55
