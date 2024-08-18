869a2158292055eecb6baa101448ed67b0d6dfb8
Anand Satheesh <anands282@gmail.com>
2024-08-18 20:44:00 +0530

    added example 2 for decorators

---
+++ decorators/example_2.py
@@ -0,0 +1,14 @@
+def log_function_call(func):
+    def wrapper(*args, **kwargs):
+        print(f"Calling function {func.__name__} with arguments {args} and {kwargs}")
+        result = func(*args, **kwargs)
+        print(f"Function {func.__name__} returned {result}")
+        return result
+    return wrapper
+
+@log_function_call
+def add(x, y):
+    return x + y
+
+add(5, 3)
+
